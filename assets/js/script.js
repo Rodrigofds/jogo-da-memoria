729e68eb5b7146d268f2c98012f539fd5584f1ef
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2021-08-25 19:04:36 -0300

    criado função para resetar variáveis auxiliares do game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let lockBoard = false;
 
 function flipCard(){
     if(lockBoard) return;
-    
+    if(this === firstCard) return;
     this.classList.toggle('flip');
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -30,13 +30,22 @@ function checkForMath(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards(){
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        resetBoard();
     }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
